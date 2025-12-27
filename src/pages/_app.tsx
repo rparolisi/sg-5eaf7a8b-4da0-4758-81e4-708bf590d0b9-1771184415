1d1aa86a24eca5040b558cbf2f7ed5f9a61d86be
softgenai <admin@softgen.ai>
2025-12-27 12:24:31 +0000

    Edit /src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import { Toaster } from "@/components/ui/toaster";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Navbar from "@/components/Navbar"; // <-- Importiamo la Navbar
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Toaster />
-    </>
-  );
+    return (
+        <div className="min-h-screen bg-gray-50">
+            {/* La Navbar è qui, quindi appare su TUTTE le pagine */}
+            <Navbar />
+
+            {/* Qui viene caricato il contenuto della pagina specifica (Home, Transazioni, ecc.) */}
+            <Component {...pageProps} />
+        </div>
+    );
 }
