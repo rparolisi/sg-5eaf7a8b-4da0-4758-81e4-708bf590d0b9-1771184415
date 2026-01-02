f68d71d6583ace340faa8c2d8da4bda2a1f742e0
softgenai <admin@softgen.ai>
2026-01-02 18:01:53 +0000

    Edit /src/components/Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -71,9 +71,9 @@ export default function Navbar() {
                             
                             {/* --- INIZIO MODIFICA LOGO --- */}
                             <div className="mr-2 relative w-10 h-10"> 
-                                {/* Assicurati che il file si chiami 'owl-logo-no_background.png' e sia nella cartella /public */}
+                                {/* Assicurati che il file si chiami 'owl-logo-no_background-new.png' e sia nella cartella /public */}
                                 <Image 
-                                    src="/owl-logo-no_background.png" 
+                                    src="/owl-logo-no_background-new.png" 
                                     alt="Owl Logo" 
                                     width={40} 
                                     height={40} 
